ca2bb7b1b7dd8d6bab334eb14490fef62232c93f
Dan Swartz <2fifty6@gmail.com>
2016-10-03 10:06:20 -0400

    sync script fix for current directory

--- sync.sh
+++ sync.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-PWD=`pwd`
+pushd `dirname $0` >/dev/null
+CURRDIR=`pwd`
 
-#if [[ ! -d ~/.ssh ]]; then
-#  mkdir ~/.ssh
-#fi
-#ln -sf $PWD/swartz_files/ssh_config ~/.ssh/config
-#ln -sf $PWD/swartz_files/gitconfig ~/.gitconfig
+ln -sf $CURRDIR/swartz_files/vimrc ~/.vimrc
+ln -sf $CURRDIR/swartz_files/zshrc ~/.zshrc
+ln -sf $CURRDIR/swartz_files/zprompt ~/.zprompt
+ln -sf $CURRDIR/swartz_files/oh-my-zshrc ~/.oh-my-zshrc
 
-ln -sf $PWD/swartz_files/vimrc ~/.vimrc
-ln -sf $PWD/swartz_files/zshrc ~/.zshrc
-ln -sf $PWD/swartz_files/zprompt ~/.zprompt
-ln -sf $PWD/swartz_files/oh-my-zshrc ~/.oh-my-zshrc
-
-[[ ! -L ~/.dotfiles/initscripts ]] && ln -sf $PWD/swartz_files/initscripts ~/.dotfiles/initscripts
+[[ ! -L ~/.dotfiles/initscripts ]] && ln -sf $CURRDIR/swartz_files/initscripts ~/.dotfiles/initscripts
