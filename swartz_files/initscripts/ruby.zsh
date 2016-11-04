c6831bffb42574ba2f77627cb141e2c0568cd189
Dan Swartz <2fifty6@gmail.com>
2016-11-04 09:32:01 -0400

    commented out ruby script

--- swartz_files/initscripts/ruby.zsh
+++ swartz_files/initscripts/ruby.zsh
@@ -1,8 +1,8 @@
 # RUBY
-alias rubyedit="vim $0"
-alias rubyrefresh="source $0"
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-alias rbinstall="RUBY_CONFIGURE_OPTS=--with-readline-dir=`brew --prefix readline` rbenv install"
-
-alias rubocopper="rubocop -SDa"
+#alias rubyedit="vim $0"
+#alias rubyrefresh="source $0"
+#export PATH="$HOME/.rbenv/bin:$PATH"
+#eval "$(rbenv init -)"
+#alias rbinstall="RUBY_CONFIGURE_OPTS=--with-readline-dir=`brew --prefix readline` rbenv install"
+#
+#alias rubocopper="rubocop -SDa"
