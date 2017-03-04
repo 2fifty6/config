07c4e342b08b25ff6e9bba11d3a0c13d33d8d71e
Dan Swartz <daniel.swartz@ellucian.com>
2017-03-04 10:22:38 -0500

    gitconfig

--- sync.sh
+++ sync.sh
@@ -4,8 +4,10 @@ pushd `dirname $0` >/dev/null
 CURRDIR=`pwd`
 
 ln -sf $CURRDIR/swartz_files/vimrc ~/.vimrc
+ln -sf $CURRDIR/swartz_files/gitconfig ~/.gitconfig
 ln -sf $CURRDIR/swartz_files/zshrc ~/.zshrc
 ln -sf $CURRDIR/swartz_files/zprompt ~/.zprompt
 ln -sf $CURRDIR/swartz_files/oh-my-zshrc ~/.oh-my-zshrc
 
+mkdir -p ~/.dotfiles
 [[ ! -L ~/.dotfiles/initscripts ]] && ln -sf $CURRDIR/swartz_files/initscripts ~/.dotfiles/initscripts
