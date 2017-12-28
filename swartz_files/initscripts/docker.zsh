3d6bffb7983166d0df6255901eb9c62b34fe13f8
Dan Swartz <2fifty6@gmail.com>
2017-12-28 12:11:04 -0500

    docker.zsh dry

--- swartz_files/initscripts/docker.zsh
+++ swartz_files/initscripts/docker.zsh
@@ -5,36 +5,31 @@ alias drefresh=dockerrefresh
 
 [[ -z "$DOCKER_PORT" ]] && DOCKER_PORT=2222
 
-function docker-centos {
-  OS="centos"
-  [[ -z $1 ]] && NAME=$OS || NAME=$1
+function docker-run {
+  OS=$1
+  NAME=$2
   docker run -h $OS --name $NAME -p$DOCKER_PORT:22 -it -d -v /home/dswartz/git/ansible:/tmp/ansible dswartz/2fifty6:$OS
   DOCKER_PORT=$((DOCKER_PORT+1))
+}
+function docker-centos {
+  OS="centos"; [[ -z $1 ]] && NAME=$OS || NAME=$1
+  docker-run $OS $NAME
+}
+function docker-fedora {
+  OS="fedora"; [[ -z $1 ]] && NAME=$OS || NAME=$1
+  docker-run $OS $NAME
+}
+function docker-debian {
+  OS="debian"; [[ -z $1 ]] && NAME=$OS || NAME=$1
+  docker-run $OS $NAME
+}
+function docker-ubuntu {
+  OS="ubuntu"; [[ -z $1 ]] && NAME=$OS || NAME=$1
+  docker-run $OS $NAME
 }
 alias dcentos=docker-centos
-
-function docker-fedora {
-  OS="fedora"
-  [[ -z $1 ]] && NAME=$OS || NAME=$1
-  docker run -h $OS --name $NAME -p$DOCKER_PORT:22 -it -d -v /home/dswartz/git/ansible:/tmp/ansible dswartz/2fifty6:$OS
-  DOCKER_PORT=$((DOCKER_PORT+1))
-}
 alias dfedora=docker-fedora
-
-function docker-debian {
-  OS="debian"
-  [[ -z $1 ]] && NAME=$OS || NAME=$1
-  docker run -h $OS --name $NAME -p$DOCKER_PORT:22 -it -d -v /home/dswartz/git/ansible:/tmp/ansible dswartz/2fifty6:$OS
-  DOCKER_PORT=$((DOCKER_PORT+1))
-}
 alias ddebian=docker-debian
-
-function docker-ubuntu {
-  OS="ubuntu"
-  [[ -z $1 ]] && NAME=$OS || NAME=$1
-  docker run -h $OS --name $NAME -p$DOCKER_PORT:22 -it -d -v /home/dswartz/git/ansible:/tmp/ansible dswartz/2fifty6:$OS
-  DOCKER_PORT=$((DOCKER_PORT+1))
-}
 alias dubuntu=docker-ubuntu
 
 function dnames { docker ps --format '{{.Names}}' }
